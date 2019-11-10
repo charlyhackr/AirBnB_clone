1d1e0b1b16fef7da10403d3862dd2d5bd283b1c6
charlyhackr <930@holbertonschool.com>
2019-11-09 20:01:41 -0500

    fix pylint

--- models/base_model.py
+++ models/base_model.py
@@ -24,14 +24,14 @@ class BaseModel():
            *args : Unused.
            **kwargs {dict}: key/value pairs of attributes.
         """
-        format = "%Y-%m-%dT%H:%M:%S.%f"
+        frmt = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if len(kwargs) != 0:
+        if not kwargs:
             for key, val in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(val, format)
+                if key in ("created_at", "updated_at"):
+                    self.__dict__[key] = datetime.strptime(val, frmt)
                 else:
                     self.__dict__[key] = val
 
