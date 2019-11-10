49383abb1364e72395484c3a2a1c297bd47f2f7e
charlyhackr <930@holbertonschool.com>
2019-11-10 15:27:19 -0500

    Add console 0.0.1

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Command Interprete for Airbnb Porject."""
+
+import cmd
+
+class HBHBCommand(cmd.Cmd):
+    """ Class for command."""
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """Exit to the program."""
+        return(True)
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return(True)
+
+if __name__ == '__main__':
+    interprete = HBHBCommand()
+    interprete.cmdloop()
