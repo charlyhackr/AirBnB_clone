b318257bd5ef926baf47b6fc2e0c5253ac6d3482
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-10 05:10:02 -0500

    (feat) file storage
    Created the File Storage class:
    
    - The instance of the File Storage class will keep every instance that is
    a subclass of BaseModel.
    - Every instance kept by the File Storage instance is serialized into JSON
    and written into a storage file.
    - The File Storage instance reloads itself using the generated storage
    file.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 import json
+import os
 """
 File Storage class.
 """
@@ -19,6 +20,25 @@ class FileStorage():
     def __init__(self):
         """ Initializes a FileStorage instance. """
         self.__objects = {}
+        self.__file_path = "storage.json"
 
     def all(self):
+        """ Returns a dictionary with all the objects of the file storage. """
         return self.__objects
+
+    def new(self, obj):
+        """ Adds a new object inside the file storage. """
+        self.__objects["{}.{}".format(obj.__class__.__name__,
+                                      obj.id)] = obj.to_dict()
+
+    def save(self):
+        """ Saves the JSON of all the objects into the JSON storage file. """
+        with open("storage.json", "w+") as f:
+            f.write(json.dumps(self.__objects))
+
+    def reload(self):
+        """ Reloads all its objects from the JSON storage file. """
+        if (os.path.exists(self.__file_path) and
+                os.path.isfile(self.__file_path)):
+            with open("storage.json", "r") as f:
+                self.__objects = json.loads(f.read())
