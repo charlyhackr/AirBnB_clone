e86617e5871cdba4565d04aa10bdb422d0256d91
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-10 09:14:36 -0500

    (test) fix of tests for storage class

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -7,6 +7,15 @@ from models.base_model import BaseModel
 
 class ExecutionTestCase(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for attr in storage.__dict__:
+            if "__objects" in attr:
+                setattr(storage, attr, {})
+        if (os.path.exists("storage.json")
+                and os.path.isfile("storage.json")):
+            os.remove("storage.json")
+
     def test_storage_application_instance(self):
         storage.save()
         with open("storage.json", "r") as f:
@@ -14,8 +23,8 @@ class ExecutionTestCase(unittest.TestCase):
         self.assertEqual({}, compare_dict)
 
     def test_storage_save_objects(self):
-        storage.new(BaseModel())
-        storage.new(BaseModel())
+        instance_1 = BaseModel()
+        instance_2 = BaseModel()
         self.assertTrue(all("BaseModel." in x for x in storage.all().keys()))
 
     def test_storage_look_for_file(self):
@@ -24,12 +33,19 @@ class ExecutionTestCase(unittest.TestCase):
                         and os.path.isfile("storage.json"))
 
     def test_storage_file_contains_objects(self):
-        storage.new(BaseModel())
-        storage.new(BaseModel())
+        instance_1 = BaseModel()
+        instance_2 = BaseModel()
         storage.save()
         with open("storage.json", "r") as f:
             compare_dict = json.loads(f.read())
         self.assertEqual(storage.all(), compare_dict)
+
+    def test_storage_update_object(self):
+        instance = BaseModel()
+        instance.foo = 42
+        instance.save()
+        with open("storage.json", "r") as f:
+            self.assertTrue("\"foo\": 42" in f.read())
 
     def cleanUp(self):
         for attr in storage.__dict__:
