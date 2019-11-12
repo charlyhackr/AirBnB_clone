22385108a5b659c6f8d265e6c1a370a44b6e19fd
charlyhackr <930@holbertonschool.com>
2019-11-12 14:00:21 -0500

    add create, shoe and all functions

--- console.py
+++ console.py
@@ -2,11 +2,23 @@
 """Command Interprete for Airbnb Porject."""
 
 import cmd
+import shlex
+import models
 
 
 class HBHBCommand(cmd.Cmd):
     """ Class for command."""
     prompt = "(hbnb) "
+
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     def do_quit(self, args):
         """Exit to the program."""
@@ -16,6 +28,76 @@ class HBHBCommand(cmd.Cmd):
         """EOF signal to exit the program."""
         print("")
         return True
+
+    def do_create(self, args):
+        """Command to create an instance of a class."""
+        args = shlex.split(args)
+        if args == []:
+            print("** class name missing **")
+        elif args[0] not in HBHBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            models.storage.reload()
+            new = eval(args[0])()
+            new.save()
+            print(new.id)
+
+    def do_show(self, args):
+        """Prints the string representation of an instance."""
+        args = shlex.split(args)
+        if args == []:
+            print("** class name missing **")
+        elif args[0] not in HBHBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            models.storage.reload()
+            for ins, obj in models.storage.all().items():
+                if obj.id == args[1] and obj.__class__.__name__ == args[0]:
+                    print(obj.__str__())
+                    return
+            print("** no instance found **")
+
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and id."""
+        args = shlex.split(args)
+        if args == []:
+            print("** class name missing **")
+        elif args[0] not in HBHBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            models.storage.reload()
+            current_objs = models.storage.all()
+            for ins, obj in current_objs.items():
+                if obj.id == args[1] and obj.__class__.__name__ == args[0]:
+                    del(current_objs[ins])
+                    models.storage.save()
+                    return
+            print("** no instance found **")
+
+   
+
+    def do_all(self, args):
+        """Command to print the string representation of all instances."""
+        args = shlex.split(args)
+        if args == []:
+            models.storage.reload()
+            new_list = []
+            for ins, obj in models.storage.all().items():
+                new_list.append(obj.__str__())
+            print(new_list)
+        elif args[0] not in HBHBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            models.storage.reload()
+            new_list = []
+            for ins, obj in models.storage.all().items():
+                if obj.__class__.__name__ == args[0]:
+                    new_list.append(obj.__str__())
+            print(new_list)
 
 
 if __name__ == '__main__':
