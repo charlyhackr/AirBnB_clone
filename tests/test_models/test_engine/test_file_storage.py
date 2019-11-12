70f68715cdd02556f0fdd388abfd64b56f5c5c64
charlyhackr <930@holbertonschool.com>
2019-11-12 15:32:12 -0500

    add directory test_engine

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,78 @@
+"""
+This file contains the tests for the FileStorage class
+"""
+import unittest
+import os
+import json
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class FileStorageTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        os.remove("storage.json")
+
+    def test_create_instance(self):
+        instance = FileStorage()
+        self.assertIsInstance(instance, FileStorage)
+
+    def test_check_type_of_file_storage_objects(self):
+        instance = FileStorage()
+        self.assertIsInstance(instance.all(), dict)
+
+    def test_add_new_object_to_file_storage(self):
+        instance = FileStorage()
+        obj = BaseModel()
+        instance.new(obj)
+        self.assertIn(obj,
+                      instance.all().values(),
+                      "object isn't in file storage")
+
+    def test_generate_file(self):
+        instance = FileStorage()
+        instance.save()
+        self.assertTrue(os.path.exists("storage.json") and
+                        os.path.isfile("storage.json"))
+
+    def test_add_new_object_and_save_to_file(self):
+        instance = FileStorage()
+        instance.new(BaseModel())
+        instance.new(BaseModel())
+        instance.save()
+        with open("storage.json", "r") as f:
+            from_json = json.loads(f.read())
+            all_instances = {key: value.to_dict()
+                             for (key, value) in instance.all().items()}
+
+        self.assertEqual(from_json, all_instances, "wrong dictionary")
+
+    def test_reload_from_file(self):
+        instance = FileStorage()
+        instance.new(BaseModel())
+        instance.new(BaseModel())
+        instance.save()
+        del instance
+        new_instance = FileStorage()
+        new_instance.reload()
+        with open("storage.json", "r") as f:
+            from_json = json.loads(f.read())
+            all_instances = {key: value.to_dict()
+                             for (key, value) in new_instance.all().items()}
+
+        self.assertEqual(from_json, all_instances, "wrong dictionary")
+
+    def test_reload_from_nonexistent_file(self):
+        instance = FileStorage()
+        instance.reload()
+        instance.save()
+        self.assertEqual({}, instance.all())
+
+    def tearDown(self):
+        for attr in FileStorage.__dict__:
+            if "__objects" in attr:
+                setattr(FileStorage, attr, {})
+        if (os.path.exists("storage.json")
+                and os.path.isfile("storage.json")):
+            os.remove("storage.json")
---
+++ tests/test_models/test_engine/test_storage.py
@@ -0,0 +1,62 @@
+"""
+This file contains the test for the `storage` `FileStorage` global instance
+"""
+import unittest
+import json
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class StorageTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for attr in storage.__class__.__dict__:
+            if "__objects" in attr:
+                setattr(storage.__class__, attr, {})
+        if (os.path.exists("storage.json")
+                and os.path.isfile("storage.json")):
+            os.remove("storage.json")
+
+    def test_storage_application_instance(self):
+        storage.save()
+        with open("storage.json", "r") as f:
+            compare_dict = json.loads(f.read())
+        self.assertEqual({}, compare_dict)
+
+    def test_storage_save_objects(self):
+        instance_1 = BaseModel()
+        instance_2 = BaseModel()
+        self.assertTrue(all("BaseModel." in x for x in storage.all()))
+
+    def test_storage_look_for_file(self):
+        storage.save()
+        self.assertTrue(os.path.exists("storage.json")
+                        and os.path.isfile("storage.json"))
+
+    def test_storage_file_contains_objects(self):
+        instance_1 = BaseModel()
+        instance_2 = BaseModel()
+        storage.save()
+        with open("storage.json", "r") as f:
+            from_json = json.loads(f.read())
+            all_instances = {key: value.to_dict()
+                             for (key, value) in storage.all().items()}
+
+        self.assertEqual(all_instances, from_json)
+
+    def test_storage_update_object(self):
+        instance = BaseModel()
+        instance.foo = 42
+        instance.save()
+        with open("storage.json", "r") as f:
+            self.assertTrue("\"foo\": 42" in f.read())
+
+    def tearDown(self):
+        for attr in storage.__class__.__dict__:
+            if "__objects" in attr:
+                setattr(storage.__class__, attr, {})
+        if (os.path.exists("storage.json")
+                and os.path.isfile("storage.json")):
+            os.remove("storage.json")
