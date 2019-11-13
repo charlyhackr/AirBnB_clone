039fa3b6072fc5d7dbf7e33306ed509a7ffc1530
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-13 14:05:04 -0500

    (fix) difference between created_at and updated_at
    Class BaseModel instances will now have the same dates created_at and
    updated_at at their moment of instanciation.

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel():
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.updated_at = self.created_at
 
         if kwargs:
             for key, val in kwargs.items():
